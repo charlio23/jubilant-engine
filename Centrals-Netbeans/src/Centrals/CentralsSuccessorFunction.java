bdcd65ec97fd2d20b9720cc33f1e2a3ab38aa866
Dean Zhu <deanzhu1@gmail.com>
2018-10-13 17:14:04 +0200

    added Netbeans Project

---
+++ Centrals-Netbeans/src/Centrals/CentralsSuccessorFunction.java
@@ -0,0 +1,12 @@
+package Centrals;
+
+import aima.search.framework.SuccessorFunction;
+
+import java.util.List;
+
+public class CentralsSuccessorFunction implements SuccessorFunction {
+
+    public List getSuccessors(Object o) {
+        return null;
+    }
+}
---
+++ Centrals-Netbeans/src/Main.java
@@ -0,0 +1,8 @@
+import Centrals.CentralsBoard;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
