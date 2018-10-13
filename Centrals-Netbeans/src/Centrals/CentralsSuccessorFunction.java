eca8f6451f1d29bbde59c3a401e81425756867a3
MCidras <cidras97@gmail.com>
2018-10-13 18:35:25 +0200

    ..

--- Centrals-Netbeans/src/Centrals/CentralsSuccessorFunction.java
+++ Centrals-Netbeans/src/Centrals/CentralsSuccessorFunction.java
@@ -1,12 +1,26 @@
 package Centrals;
 
 import aima.search.framework.SuccessorFunction;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class CentralsSuccessorFunction implements SuccessorFunction {
 
     public List getSuccessors(Object o) {
-        return null;
+        CentralsBoard board = (CentralsBoard) o;
+        List succesors = new ArrayList();
+        
+        for (int id = 0; id < board.getClientesSize(); ++id) {
+            int jd;
+            if (board.isGuaranteed) jd = 0;
+            else jd = -1;
+            for (jd; jd < getCentralesSize(); ++jd) {
+                CentralsBoard newBoard = board.getCopy();
+                newBoard.changeCentral(id, jd);
+                successors.add(new Successor(id + " + " + jd, newBoard));
+            }
+        }
+        
+        return succesors;
     }
 }
