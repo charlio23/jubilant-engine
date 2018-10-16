7618c439a501423a9d5b52096a88fa06f6bab13d
charlio23 <carlesbalsellsrodas@hotmail.com>
2018-10-16 12:54:03 +0200

    Heuristic functions

--- Centrals/CentralsBoard.java
+++ Centrals/CentralsBoard.java
@@ -117,51 +117,6 @@ public class CentralsBoard {
     private CentralsBoard() {
     }
 
-    //TODO: Change heuristic funtion to reflect penalizations
-    public double heuristicFunction() {
-        //Ganancia
-        double ganancia = 0;
-
-        //Centrales
-        double[] suministro = new double[centrales.size()];
-        for(int i = 0; i < suministro.length; ++i) {
-            suministro[i] = 0.0;
-        }
-        for (int id = 0; id < state.length; ++id) {
-            if (state[id] != -1)
-                try {
-                    suministro[state[id]] += clientes.get(id).getConsumo() / (1 - perdida(id, state[id]));
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-        }
-        for (int jd = 0; jd < centrales.size(); ++jd) {
-            if (suministro[jd] > 0) ganancia -= costeMarcha(jd);
-            else ganancia -= costeParada(jd);
-        }
-
-        //Clientes
-        for (int id = 0; id < state.length; ++id) {
-            try{
-                if (state[id] != -1) ganancia += precioTarifa(id);
-                else ganancia -= VEnergia.getTarifaClientePenalizacion(clientes.get(id).getContrato())*clientes.get(id).getConsumo();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-        for(int jd = 0; jd < centrales.size(); ++jd) {
-            double overflow = suministro[jd] - centrales.get(jd).getProduccion();
-            if(overflow > 0) {
-                overflow = Math.max(overflow, 1.0);
-                ganancia -= overflow * overflow * MAXBENEFIT * centrales.size();
-            }
-
-        }
-
-        return -ganancia;
-    }
 
 
     public boolean allGarantizados() {
@@ -286,5 +241,8 @@ public class CentralsBoard {
         for (int stat: state) {
             System.out.print(' ' + String.valueOf(stat));
         }
+    }
+
+    public double[] getCentrales() {
     }
 }
--- Centrals/CentralsHeuristicFunction.java
+++ Centrals/CentralsHeuristicFunction.java
@@ -1,10 +1,56 @@
 package Centrals;
 
+import IA.Energia.VEnergia;
 import aima.search.framework.HeuristicFunction;
 
 public class CentralsHeuristicFunction implements HeuristicFunction {
 
     public double getHeuristicValue(Object o) {
-        return ((Centrals.CentralsBoard) o).heuristicFunction();
+        return heuristicFunction((CentralsBoard) o);
     }
+
+    public double heuristicFunction(CentralsBoard board) {
+        //Ganancia
+        double ganancia = 0;
+        //Centrales
+        double[] suministro = new double[centrales.size()];
+        for(int i = 0; i < suministro.length; ++i) {
+            suministro[i] = 0.0;
+        }
+        for (int id = 0; id < state.length; ++id) {
+            if (state[id] != -1)
+                try {
+                    suministro[state[id]] += clientes.get(id).getConsumo() / (1 - perdida(id, state[id]));
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+        }
+        for (int jd = 0; jd < centrales.size(); ++jd) {
+            if (suministro[jd] > 0) ganancia -= costeMarcha(jd);
+            else ganancia -= costeParada(jd);
+        }
+
+        //Clientes
+        for (int id = 0; id < state.length; ++id) {
+            try{
+                if (state[id] != -1) ganancia += precioTarifa(id);
+                else ganancia -= VEnergia.getTarifaClientePenalizacion(clientes.get(id).getContrato())*clientes.get(id).getConsumo();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        for(int jd = 0; jd < centrales.size(); ++jd) {
+            double overflow = suministro[jd] - centrales.get(jd).getProduccion();
+            if(overflow > 0) {
+                overflow = Math.max(overflow, 1.0);
+                ganancia -= overflow * overflow * MAXBENEFIT * centrales.size();
+            }
+
+        }
+
+        return -ganancia;
+    }
+
 }
