5f2735f8478fdb23826b7efb4c287ba3b9cf33d5
cescmentationfolch <cfolchaldehuelo@gmail.com>
2018-10-16 13:13:09 +0200

    anar fent heuristic

--- Centrals/CentralsBoard.java
+++ Centrals/CentralsBoard.java
@@ -11,8 +11,8 @@ import java.util.Random;
 public class CentralsBoard {
 
     private int[] state;
-    private static Centrales centrales;
-    private static Clientes clientes;
+    public static Centrales centrales;
+    public static Clientes clientes;
 
     public static final int GARANTIZADO = 0;
     public static final int NOGARANTIZADO = 1;
@@ -161,73 +161,6 @@ public class CentralsBoard {
             return true;
         }
         return false;
-    }
-
-    ////// Auxiliares
-
-    // Pre: clId es el identificador de un cliente
-    //    centId es el identificador de una central
-    private double distance(int clId, int centId) {
-        Cliente cliente = clientes.get(clId);
-        Central central = centrales.get(centId);
-        double dx = cliente.getCoordX()-central.getCoordX();
-        double dy = cliente.getCoordY()-central.getCoordY();
-        return Math.sqrt(dx*dx + dy*dy);
-    }
-
-    // Pre: clId es el identificador de un cliente
-    //    centId es el identificador de una central
-    private double perdida(int clId, int centId) {
-        return VEnergia.getPerdida(distance(clId, centId));
-    }
-
-    // Pre: centId es el identificador de una central
-    private double costeMarcha(int centId) {
-        Central central = centrales.get(centId);
-        try {
-            return VEnergia.getCosteMarcha(central.getTipo()) + VEnergia.getCosteProduccionMW(central.getTipo())*central.getProduccion();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return 0;
-    }
-
-    // Pre: centId es el identificador de una central
-    private double costeParada(int centId) {
-        Central central = centrales.get(centId);
-        try{
-            return VEnergia.getCosteParada(central.getTipo());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return 0;
-    }
-
-    // Pre: clId es el identificador de un cliente
-    private double precioTarifa(int clId) {
-        Cliente cliente = clientes.get(clId);
-        try {
-            if (cliente.getTipo() == GARANTIZADO) {
-                return VEnergia.getTarifaClienteGarantizada(cliente.getContrato())*cliente.getConsumo();
-            } else {
-                return VEnergia.getTarifaClienteNoGarantizada(cliente.getContrato())*cliente.getConsumo();
-            }
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return 0;
-    }
-
-    public int getClientesSize() {
-        return clientes.size();
-    }
-
-    public int getCentralesSize() {
-        return centrales.size();
-    }
-
-    public boolean isGuaranteed(int id) {
-        return clientes.get(id).getTipo() == GARANTIZADO;
     }
 
     public CentralsBoard getCopy() {
--- Centrals/CentralsHeuristicFunction.java
+++ Centrals/CentralsHeuristicFunction.java
@@ -3,54 +3,81 @@ package Centrals;
 import IA.Energia.VEnergia;
 import aima.search.framework.HeuristicFunction;
 
-public class CentralsHeuristicFunction implements HeuristicFunction {
+public abstract class CentralsHeuristicFunction implements HeuristicFunction {
 
-    public double getHeuristicValue(Object o) {
-        return heuristicFunction((CentralsBoard) o);
+	public double getHeuristicValue(Object o) {
+		return heuristicFunction((CentralsBoard) o);
+	}
+
+	abstract double heuristicFunction(CentralsBoard board);
+
+    ////// Auxiliares
+
+    // Pre: clId es el identificador de un cliente
+    //    centId es el identificador de una central
+    private double distance(int clId, int centId) {
+	Cliente cliente = clientes.get(clId);
+	Central central = centrales.get(centId);
+	double dx = cliente.getCoordX()-central.getCoordX();
+	double dy = cliente.getCoordY()-central.getCoordY();
+	return Math.sqrt(dx*dx + dy*dy);
     }
 
-    public double heuristicFunction(CentralsBoard board) {
-        //Ganancia
-        double ganancia = 0;
-        //Centrales
-        double[] suministro = new double[centrales.size()];
-        for(int i = 0; i < suministro.length; ++i) {
-            suministro[i] = 0.0;
-        }
-        for (int id = 0; id < state.length; ++id) {
-            if (state[id] != -1)
-                try {
-                    suministro[state[id]] += clientes.get(id).getConsumo() / (1 - perdida(id, state[id]));
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-        }
-        for (int jd = 0; jd < centrales.size(); ++jd) {
-            if (suministro[jd] > 0) ganancia -= costeMarcha(jd);
-            else ganancia -= costeParada(jd);
-        }
-
-        //Clientes
-        for (int id = 0; id < state.length; ++id) {
-            try{
-                if (state[id] != -1) ganancia += precioTarifa(id);
-                else ganancia -= VEnergia.getTarifaClientePenalizacion(clientes.get(id).getContrato())*clientes.get(id).getConsumo();
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-            }
-        }
-
-        for(int jd = 0; jd < centrales.size(); ++jd) {
-            double overflow = suministro[jd] - centrales.get(jd).getProduccion();
-            if(overflow > 0) {
-                overflow = Math.max(overflow, 1.0);
-                ganancia -= overflow * overflow * MAXBENEFIT * centrales.size();
-            }
-
-        }
-
-        return -ganancia;
+    // Pre: clId es el identificador de un cliente
+    //    centId es el identificador de una central
+    private double perdida(int clId, int centId) {
+	return VEnergia.getPerdida(distance(clId, centId));
     }
+
+    // Pre: centId es el identificador de una central
+    private double costeMarcha(int centId) {
+	Central central = centrales.get(centId);
+	try {
+	    return VEnergia.getCosteMarcha(central.getTipo()) + VEnergia.getCosteProduccionMW(central.getTipo())*central.getProduccion();
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
+	}
+	return 0;
+    }
+
+    // Pre: centId es el identificador de una central
+    private double costeParada(int centId) {
+	Central central = centrales.get(centId);
+	try{
+	    return VEnergia.getCosteParada(central.getTipo());
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
+	}
+	return 0;
+    }
+
+    // Pre: clId es el identificador de un cliente
+    private double precioTarifa(int clId) {
+	Cliente cliente = clientes.get(clId);
+	try {
+	    if (cliente.getTipo() == GARANTIZADO) {
+		return VEnergia.getTarifaClienteGarantizada(cliente.getContrato())*cliente.getConsumo();
+	    } else {
+		return VEnergia.getTarifaClienteNoGarantizada(cliente.getContrato())*cliente.getConsumo();
+	    }
+	} catch (Exception e) {
+	    System.out.println(e.getMessage());
+	}
+	return 0;
+    }
+
+    public int getClientesSize() {
+	return clientes.size();
+    }
+
+    public int getCentralesSize() {
+	return centrales.size();
+    }
+
+    public boolean isGuaranteed(int id) {
+	return clientes.get(id).getTipo() == GARANTIZADO;
+    }
+
+	
 
 }
